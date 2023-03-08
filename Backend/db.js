8b61f45872360024290933a354d2526fa356484a
Niketan <niketanb1234@gmail.com>
2023-03-09 00:03:48 +0530

    Revert "Ghya"
    This reverts commit c2612ae75f6729957392966716b68fd905d00cad.

--- Backend/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose=require('mongoose');
-const mongoUri='mongodb+srv://Niketan:pass123@cluster0.3cv8tnw.mongodb.net/?retryWrites=true&w=majority';
-// const mongoUri='mongodb://localhost:27017'
-const connectToMongo=()=>{
-    console.log("hi");
-    mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
-}
-module.exports =connectToMongo;
